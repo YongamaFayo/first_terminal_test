8a6ef146e95698258a6ccf34ba83c041e472b32e
yongama <yongamafayo@gmail.com>
2020-08-12 15:36:28 +0200

    first commit

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -12,11 +12,11 @@ describe('regCheck', function () {
     });
 
     it('should return true if registration ends with loc', function () {
-        assert.equal(false, regCheck('DC 55 YU GP'));
+        assert.equal(true, regCheck('DC 55 YU GP', "GP"));
     });
 
-    it('should return true if registration is not from GP loc', function () {
-        assert.equal(false, regCheck('5566 L'));
+    it('should return true if registration is from L loc', function () {
+        assert.equal(true, regCheck('5566 L',"L"));
     });
 
 
